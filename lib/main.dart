53af1974ee348ac276c547168719f034967be1a1
LeonardoVieiraM <leonardovieiramachado@gmail.com>
2025-10-25 17:42:50 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'screens/task_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager Pro',
+      debugShowCheckedModeBanner: false,
+      
+      // Tema Claro
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      
+      // Tema Escuro
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      
+      // Seguir configuração do sistema
+      themeMode: ThemeMode.system,
+      
+      home: const TaskListScreen(),
+    );
+  }
+}
---
+++ lib/models/category.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+class Category {
+  final String id;
+  final String name;
+  final Color color;
+
+  Category({
+    String? id,
+    required this.name,
+    required this.color,
+  }) : id = id ?? const Uuid().v4();
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'color': color.value,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'],
+      name: map['name'],
+      color: Color(map['color']),
+    );
+  }
+}
+
+final List<Category> defaultCategories = [
+  Category(name: 'Trabalho', color: Colors.blue),
+  Category(name: 'Pessoal', color: Colors.green),
+  Category(name: 'Estudos', color: Colors.orange),
+  Category(name: 'Saúde', color: Colors.red),
+  Category(name: 'Lazer', color: Colors.purple),
+  Category(name: 'Outros', color: Colors.grey),
+];
